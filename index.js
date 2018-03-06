d386cf8980b5e627d85b5c99b14b2c66bf2e2862
Greg <greg.scott@y7mail.com>
2018-03-06 20:01:05 +1100

    Adding skeleton of application from demonstration code.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("Go go go go go go go go go");
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-//Test adding a line
+console.log("Wouldn't you like to be a Pepper, too?");
+
+app.use(express.static(__dirname + "/public"));
+
+
+function onConnection(inSocket)
+{
+    console.log("Howdy, Socket: " + inSocket.id);
+
+    inSocket.on('drawing', (data) => inSocket.broadcast.emit('drawing', data));
+
+}
+
+
+io.on('connectio', onConnection);
+
+http.listen(3000, () => console.log("I am alive..."))
