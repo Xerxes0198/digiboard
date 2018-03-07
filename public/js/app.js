ab0f690ca81900e30faee1c151a7fd9f7712b450
Greg <greg.scott@y7mail.com>
2018-03-07 21:17:40 +1100

    Updates to app.js client side.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,36 @@
 (
     function()
     {
+        var drawing = false;
         var socket = io();
         console.log("fsdgs");
 
         var canvas = document.getElementById('whiteboard');
         var context = canvas.getContext('2d');
+
+        canvas.addEventListener('mousedown', onMouseDown, false);
+        canvas.addEventListener('mousemove', onMouseMove, false);
+        canvas.addEventListener('mouseup', onMouseUp, false);
+
+
+        function onMouseDown(e)
+        {
+            drawing = true;
+        }
+
+        function onMouseMove(e)
+        {
+            if(drawing)
+            {
+                console.log(e.clientX + " : " + e.clientY);
+            }
+
+        }
+
+        function onMouseUp(e)
+        {
+            drawing = false;
+        }
 
     }
 )();
